2e9252bca417ea7cc7115b4e786278e937fdda25
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-13 02:05:04 +0100

    exo7 gotov

---
+++ ex07/src/index.js
@@ -0,0 +1,8 @@
+function main() {
+    var someAdjective = "the best.";
+    var myString = "Mistral is ";
+    myString += someAdjective;
+    return myString;
+}
+console.log(main());
+module.exports = main;
