5b7e17f9ec6b4cce2fa9a850c287584bbbd81ccf
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-13 02:00:33 +0100

    ex06 gotov

---
+++ ex06/src/index.js
@@ -0,0 +1,7 @@
+function main() {
+    var myName = "Harry Potter";
+    var myString = "My name is " + myName + " and I am well!";
+    return myString;
+}
+console.log(main());
+module.exports = main;
