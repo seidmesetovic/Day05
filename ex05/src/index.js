19d073dc1b1a29d6834ce398620070f01747a1da
seidmesetovic <seid.mesetovic@gmail.com>
2021-03-13 01:53:29 +0100

    Gotov i ovaj

---
+++ ex05/src/index.js
@@ -0,0 +1,7 @@
+function main() {
+    var myString = "Concatenation is the process of appending ";
+    myString += "onestring to the end of the another string."
+    return myString;
+}
+console.log(main());
+module.exports = main;
